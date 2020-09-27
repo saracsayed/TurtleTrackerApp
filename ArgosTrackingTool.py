647f3022c5ac5ce5aed0018275b0b5bcc14ffa3a
saracsayed <saracsayed@yahoo.com>
2020-09-27 16:17:53 -0400

    Filters out lc not in 1,2,3

--- ArgosTrackingTool.py
+++ ArgosTrackingTool.py
@@ -18,25 +18,31 @@ file_object = open(file_name, 'r')
 # Read contents of file into a list
 line_list = file_object.readlines()
 
+#Close the file
 file_object.close()
+
+#Create two empty dictionary objects
+date_dict={}
+coord_dict={} 
 
 #Iterate using a for loop
 for lineString in line_list:
-    if lineString[0] == "#" or lineString[0] == 'u': continue
-
-    # Use the split command to parse the items in lineString into a list object
+    if lineString[0] in ("#", "u"): continue
+       
+    #Use the split command to parse the items in lineString into a list object
     lineData = lineString.split()
     
     # Assign variables to specfic items in the list
     record_id = lineData[0]             # ARGOS tracking record ID
-    obs_date = lineData[2]   # Observation date
-    ob_lc = lineData[4]                 # Observation Location Class
+    obs_date = lineData[2]              # Observation date
+    obs_lc = lineData[4]                 # Observation Location Class
+     #if obs_lc not in ("1", "2", "3"):
+         #continue
     obs_lat = lineData[6]               # Observation Latitude
     obs_lon = lineData[7]               # Observation Longitude
     
-    # Print information to the use
-    print (f"Record {record_id} indicates Sara was seen at {obs_lat}N and {obs_lon}W on {obs_date}")
-    
-
-#Close the file
-file_object.close()
+    # Print the location of Sara if lc is 1, 2 or 3
+    if obs_lc in ("1","2", "3"):
+        print (f"Record {record_id} indicates Sara was seen at {obs_lat}N and {obs_lon}W on {obs_date}")
+        date_dict[record_id] = obs_date
+        coord_dict[record_id] = (obs_lat,obs_lon)
